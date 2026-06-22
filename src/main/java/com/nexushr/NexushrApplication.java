ed01752c2210ec982105e9a72afdc36101fd7427
Nidhi\Nidhi Sajankar <nidhisajankar@gmail.com>
2026-06-22 09:57:07 +0530

    Completed Department CRUD Module

--- src/main/java/com/nexushr/NexushrApplication.java
+++ src/main/java/com/nexushr/NexushrApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NexushrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NexushrApplcation.class, args);
+		SpringApplication.run(NexushrApplication.class, args);
 	}
 
 }
