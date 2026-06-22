53a0eaa96dece9a28c4e2bd3ecdd60cc121528e2
Nidhi\Nidhi Sajankar <nidhisajankar@gmail.com>
2026-06-22 09:30:44 +0530

    Completed Employee CRUD Module

--- src/main/java/com/nexushr/controller/EmployeeController.java
+++ src/main/java/com/nexushr/controller/EmployeeController.java
@@ -25,4 +25,24 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(
+            @PathVariable Long id,
+            @RequestBody Employee employee) {
+
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+
+        employeeService.deleteEmployee(id);
+        return "Employee Deleted Successfully";
+    }
 }
--- src/main/java/com/nexushr/service/EmployeeService.java
+++ src/main/java/com/nexushr/service/EmployeeService.java
@@ -22,4 +22,32 @@ public class EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Not Found"));
+    }
+
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Not Found"));
+
+        employee.setEmployeeId(updatedEmployee.getEmployeeId());
+        employee.setFullName(updatedEmployee.getFullName());
+        employee.setEmail(updatedEmployee.getEmail());
+        employee.setDepartment(updatedEmployee.getDepartment());
+        employee.setDesignation(updatedEmployee.getDesignation());
+        employee.setSalary(updatedEmployee.getSalary());
+
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Not Found"));
+
+        employeeRepository.delete(employee);
+    }
 }
